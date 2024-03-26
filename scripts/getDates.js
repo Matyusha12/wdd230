5b3314210fa2dc17dfbdcf7260fac7579637a5ac
Matyusha12 <nataliyayakovleva90@icloud.com>
2024-03-25 22:12:42 -0600

    commit all

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,3 +3,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
 });
 
+document.getElementById('dark-mode-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+});
