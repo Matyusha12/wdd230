667278eb5036b23771db972db389cedf6285886c
Matyusha12 <nataliyayakovleva90@icloud.com>
2024-03-30 21:17:51 -0600

    commit

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,7 +1,22 @@
-// JavaScript to set the current year and the last modified date
 document.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById('currentYear').textContent = new Date().getFullYear();
-    document.getElementById('lastModified').textContent = document.lastModified;
+    const currentYearElement = document.getElementById('currentYear');
+    const lastModifiedElement = document.getElementById('lastModified');
+    const darkModeToggle = document.getElementById('dark-mode-toggle');
+    const hamburger = document.querySelector('.hamburger');
+    const navigation = document.getElementById('navigation');
+
+    currentYearElement.textContent = new Date().getFullYear();
+    lastModifiedElement.textContent = document.lastModified;
+
+    darkModeToggle.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode');
+    });
+
+    hamburger.addEventListener('click', () => {
+        navigation.classList.toggle('open');
+        hamburger.textContent = hamburger.textContent === '☰' ? '✖' : '☰';
+    });
 });
+
 
 
