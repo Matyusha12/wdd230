fde80179cd8564feb307bded71e8319e5d0743f8
Matyusha12 <nataliyayakovleva90@icloud.com>
2024-04-11 01:46:57 -0600

    commit

--- chamber/scripts/dates.js
+++ chamber/scripts/dates.js
@@ -30,3 +30,39 @@ document.getElementById('list-view-btn').addEventListener('click', () => {
     document.getElementById('members-container').classList.add('list-view');
     document.getElementById('members-container').classList.remove('grid-view');
 });
+
+async function fetchWeatherData() {
+    const apiKey = 'c3f9a242eeaf09eaa796aa4b837e6881';
+    const city = 'Logan,USA'; 
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        displayWeatherData(data);
+    } catch (error) {
+        console.error('Failed to fetch weather data:', error);
+    }
+}
+
+function displayWeatherData(data) {
+    const temperature = document.getElementById('current-temperature');
+    const description = document.getElementById('current-weather-description');
+
+
+    temperature.textContent = `Current Temperature: ${data.main.temp}°C`;
+    description.textContent = `Weather: ${data.weather[0].description}`;
+}
+
+fetchWeatherData();
+
+document.addEventListener('DOMContentLoaded', () => {
+    const today = new Date().getDay();
+    if (today >= 1 && today <= 3) {
+        document.getElementById('meet-greet-banner').style.display = 'block';
+    }
+});
+
+function closeBanner() {
+    document.getElementById('meet-greet-banner').style.display = 'none';
+}
